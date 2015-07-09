649aedefce3e638bdfec4e903c7d7fb4d3def4fb
Vincent Isambart <vincent-isambart@cookpad.com>
2015-07-09 09:18:30 +0900

    change with a linker error

---
+++ dokumi-test-ios/Abcd.h
@@ -0,0 +1,15 @@
+//
+//  Abcd.h
+//  dokumi-test-ios
+//
+//  Created by Vincent Isambart on 7/9/15.
+//  Copyright (c) 2015 Cookpad Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Abcd : NSObject
+
++ (void)foo;
+
+@end
